24af483ff53ecdeecd3c9be9cb8f0cde5f9cc921
Farah Khan <60636050+farahkhan10@users.noreply.github.com>
2020-10-10 16:40:59 -0400

    Update try.groovy

--- Jenkinsfile/try.groovy
+++ Jenkinsfile/try.groovy
@@ -1,1 +1,13 @@
+@Library('farahkhan10-library@master') _
+
+pipeline {
+    agent any
+    stages {
+        stage('build') {
+            steps {
+                HelloWorld 'testing'
+            }
+        }
+    }
+}
 
