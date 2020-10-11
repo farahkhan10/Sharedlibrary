1e6a65b1c884d6cef8a333eec4fe019657ad0531
Farah Khan <60636050+farahkhan10@users.noreply.github.com>
2020-10-10 21:42:51 -0400

    Update Try.groovy

--- vars/Try.groovy
+++ vars/Try.groovy
@@ -1,11 +1,11 @@
 def call(int createPipeline) {
     pipeline {
-	    agent any
-	    environment {
-	      username = 'fkhan'
+	agent any
+	environment {
+	    username = 'fkhan'
 	}
-	  stages {
-	   stage('checkout') {
+	stages {
+	  stage('checkout') {
 	    steps {
                   echo 'checkout'
                   //echo username
@@ -22,6 +22,100 @@ def call(int createPipeline) {
 		sh 'ls -la'
 	      }
            }
-	  }
-    }
+          stage('syntax_check') {
+	     parallel {
+		stage('lint') {
+		   steps {
+		      echo 'Lint'
+		      sh 'ls -la'
+		      sh 'echo $username'
+                   }
+                 }
+                 stage('security_scan') {
+		    when {
+			branch 'develop'
+		    }
+                    steps {
+                      echo "Security Scan"
+                   }
+                }
+              }
+            }
+            stage('unit_testing') {
+              when {
+	         branch 'develop'
+              }
+              steps {
+                  echo "Unit Testing"
+              }
+           }
+              stage('version') {
+                 when {
+	           branch 'develop'
+                 }
+	         steps {
+                    echo 'version'
+                 }
+              }
+              stage('package') {
+		steps {
+	          echo 'build'
+                }
+              }
+              stage('publish') {
+	        steps {
+                   echo "Push Artifacts"
+		}
+	      }
+              stage('docker_build') {
+                 parallel {
+                    stage('docker_lint') {
+                       steps {
+                          echo "Docker Lint"
+                       }
+                    }
+                     stage('image_build') {
+                        steps {
+	 		   echo "Docker Build"
+                      }
+                    }
+                  }
+               }
+                       stage('docker_tags') {
+                         steps {
+                            echo "Docker Tags"
+                        }
+                    }
+                        stage('helm_lint') {
+                          steps {
+                             echo "Helm Lint"
+                         }
+                     }
+                         stage('Deployment') {
+                          parallel {
+                             stage('Development') {
+                               when {
+                                  branch 'develop'
+                               }
+                              steps {
+                                 echo "Deploy Development Environment"
+                                }
+                              }
+                            stage('Production') {
+                              when {
+                                 branch 'Production'
+                              }
+                              steps {
+                                 echo "Deploy Production Environment"
+			      }
+                           }
+                             stage('developer') {
+ 				steps {
+				   echo "Developer Code Execution"
+				}
+			    }
+			}
+		}
+	    }
+	}
 }
